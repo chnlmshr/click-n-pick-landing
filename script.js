cb8c29e1e669a6f6c942c203025dadf1ccffd665
19bcs1114 <19bcs1114@gmail.com>
2022-05-06 13:13:07 +0530

    rotating text

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const CircularText = document.getElementById("circular-text");
-CircularText.innerHTML = CircularText.textContent.replace(
-  /\S/g,
-  "<span>$&</span>"
-);
+const CircularText = document.getElementById("circular-text-h1");
+// CircularText.innerHTML = CircularText.textContent.replace(
+//   /(.*)/,
+//   "<span>$&</span>"
+// );
+
+let elems = "EXPLORE CLICK & PICK • EXPLORE CLICK & PICK •"
+  .split("")
+  .map((char) => `<span>${char}</span>`);
+
+let s = "";
+
+for (let elem of elems) {
+  s += elem;
+}
+
+CircularText.innerHTML = s;
 
 const ele = document.querySelectorAll("span");
 
-for (var i = 1; i < ele.length; i++) {
-  ele[i].style.transform = "rotate(" + i * 16.5 + "deg)";
+for (var i = 0; i < ele.length; i++) {
+  ele[i].style.transform = "rotate(" + i * 8 + "deg)";
+  ele[i].style.margin = "5px";
 }
+// bhai ye angle dena pdega calculate krke
+// EXPLORE CLICK & PICK • EXPLORE CLICK & PICK •
+//ban gya bhai text ab gol gol ghumana hai ek point pe
